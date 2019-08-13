b6ee389f46add6e70bd18af55de3d6b680ff4c75
Jon Lefcheck <lefcheckj@si.edu>
2019-08-13 09:38:10 -0400

    Minor edits

--- R/covstop.R
+++ R/covstop.R
@@ -1,65 +1,119 @@
-#' Covstop: a function to implement coverage-based stopping
+#' covstop: a function to implement coverage-based stopping
 #' 
-#' @param comm a species-by-site data.frame or list
+#' @param comm a site (rows)-by-species (columns) data.frame
+#' @param type whether the output should be in number of individuals ("individuals") or in
+#' number of sampling units ("samples")
 #' @param Cn desired level of coverage
+#' @param knots a number of knots of computation, default is 40
 #' 
-#' @return the sample size (number of individuals) required to meet the desired
-#' level of coverage
+#' @return the number of individuals or samples required to meet the desired level of coverage
 #' 
-covstop <- function(comm, Cn = 0.90) {
+covstop <- function(comm, type = "samples", Cn = 0.90, knots = 40) {
   
   if(Cn > 1) stop("Coverage level must be between (0, 1)")
   
-  if(!is.list(comm)) comm <- split(comm, 1:ncol(comm))
+  if(type == "samples") comm <- data.frame(N = nrow(comm), t(colSums(comm > 0)))
   
-  sapply(comm, function(Spec) {
+  comm <- split(comm, 1:nrow(comm))
+  
+  sc <- sapply(comm, function(Spec) {
     
-    n <- sum(Spec)
+    Spec <- as.numeric(Spec)
     
-    endpoint <- knots <- 2*sum(Spec)
-    
-    # if (is.null(m)) {
+    endpoint <- 2 * sum(Spec)
+  
+    if(type == "individuals") {
+      
+      n <- sum(Spec)
+      
       if (endpoint <= n) {
-        m <- floor(seq(1, endpoint, length = floor(knots)))
-      } else {
-        m <- c(floor(seq(1, sum(Spec) - 1, length.out = floor(knots/2) - 
-                           1)), sum(Spec), floor(seq(sum(Spec) + 1, to = endpoint, 
-                                                     length.out = floor(knots/2))))
-      }
+        
+          m <- floor(seq(1, endpoint, length = floor(knots)))
+          
+        } else {
+          
+          m <- c(floor(seq(1, sum(Spec) - 1, length.out = floor(knots/2) - 1)), sum(Spec), floor(seq(sum(Spec) + 1, to = endpoint, length.out = floor(knots/2))))
+          
+          }
+      
       m <- c(1, m[-1])
-    # }
-    
-    Chat.Ind <- function (x, m) 
-    {
-      x <- x[x > 0]
-      n <- sum(x)
-      f1 <- sum(x == 1)
-      f2 <- sum(x == 2)
-      f0.hat <- ifelse(f2 == 0, (n - 1)/n * f1 * (f1 - 1)/2, (n - 
-                                                                1)/n * f1^2/2/f2)
-      A <- ifelse(f1 > 0, n * f0.hat/(n * f0.hat + f1), 1)
-      Sub <- function(m) {
-        if (m < n) {
-          xx <- x[(n - x) >= m]
-          out <- 1 - sum(xx/n * exp(lgamma(n - xx + 1) - lgamma(n - 
-                                                                  xx - m + 1) - lgamma(n) + lgamma(n - m)))
-        }
-        if (m == n) 
-          out <- 1 - f1/n * A
-        if (m > n) 
-          out <- 1 - f1/n * A^(m - n + 1)
-        out
-      }
-      sapply(m, Sub)
+      
+      C <- Chat.Ind(Spec, m)
+        
+    } else if(type == "samples") {
+      
+      nT <- Spec[1]
+      
+      if(endpoint <= nT) {
+        
+          m <- floor(seq(1, endpoint, length.out = floor(knots)))
+          
+          } else {
+            
+            m <- c(floor(seq(1, nT - 1, length.out = floor(knots/2) - 1)), nT, floor(seq(nT + 1, to = endpoint, length.out = floor(knots/2))))
+            
+            }
+      
+        m <- c(1, m[-1])
+ 
+      C <- Chat.Sam(Spec, m)
+      
     }
     
-    cc <- cbind.data.frame(
-      n = m,
-      C = Chat.Ind(Spec, m)
-    )
     
-    cc[which.min(cc$C <= Cn), "n"]
+    m[which.min(C <= Cn)]
     
   } )
   
+  paste0("Number of ", type, " needed to reach ", Cn*100, "% coverage: ", as.numeric(sc[which.max(sc)]))
+  
+  
 }
+
+### helper functions from iNEXT 
+### credit: T.C. Hsieh
+Chat.Ind <- function (x, m) {
+  x <- x[x > 0]
+  n <- sum(x)
+  f1 <- sum(x == 1)
+  f2 <- sum(x == 2)
+  f0.hat <- ifelse(f2 == 0, (n - 1)/n * f1 * (f1 - 1)/2, (n - 
+                                                            1)/n * f1^2/2/f2)
+  A <- ifelse(f1 > 0, n * f0.hat/(n * f0.hat + f1), 1)
+  Sub <- function(m) {
+    if (m < n) {
+      xx <- x[(n - x) >= m]
+      out <- 1 - sum(xx/n * exp(lgamma(n - xx + 1) - lgamma(n - 
+                                                              xx - m + 1) - lgamma(n) + lgamma(n - m)))
+    }
+    if (m == n) 
+      out <- 1 - f1/n * A
+    if (m > n) 
+      out <- 1 - f1/n * A^(m - n + 1)
+    out
+  }
+  sapply(m, Sub)
+}
+
+Chat.Sam <- function(x, t){
+  nT <- x[1]
+  y <- x[-1]
+  y <- y[y>0]
+  U <- sum(y)
+  Q1 <- sum(y == 1)
+  Q2 <- sum(y == 2)
+  Q0.hat <- ifelse(Q2 == 0, (nT - 1) / nT * Q1 * (Q1 - 1) / 2, (nT - 1) / nT * Q1 ^ 2/ 2 / Q2)  #estimation of unseen species via Chao2
+  A <- ifelse(Q1>0, nT*Q0.hat/(nT*Q0.hat+Q1), 1)
+  Sub <- function(t){
+    if(t < nT) {
+      yy <- y[(nT-y)>=t]
+      out <- 1 - sum(yy / U * exp(lgamma(nT-yy+1)-lgamma(nT-yy-t+1)-lgamma(nT)+lgamma(nT-t)))     
+    }
+    #if(t < nT) out <- 1 - sum(y / U * exp(lchoose(nT - y, t) - lchoose(nT - 1, t)))
+    if(t == nT) out <- 1 - Q1 / U * A
+    if(t > nT) out <- 1 - Q1 / U * A^(t - nT + 1)
+    out
+  }
+  sapply(t, Sub)		
+}
+
