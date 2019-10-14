d07964931ece9297e131c873c9d54ba77d21fbbc
Jon Lefcheck <LefcheckJ@si.edu>
2019-10-14 10:43:06 -0400

    Started to add bootstrapping

--- R/covstop.R
+++ R/covstop.R
@@ -4,11 +4,21 @@
 #' @param type whether the output should be in number of individuals ("individuals") or in
 #' number of sampling units ("samples")
 #' @param Cn desired level of coverage
+#' @param se whether bootstrapped standard errors should be returned
 #' @param knots a number of knots of computation, default is 40
 #' 
 #' @return the number of individuals or samples required to meet the desired level of coverage
 #' 
-covstop <- function(comm, type = "samples", Cn = 0.90, knots = 40) {
+#' @examples
+#' comm <- sample(100)
+#' 
+#' comm[sample(1:100, 50, replace = F)] <- 0
+#' 
+#' comm <- matrix(comm, nrow = 10)
+#' 
+#' covstop(comm)
+#' 
+covstop <- function(comm, type = "samples", Cn = 0.90, se = FALSE, knots = 40) {
   
   if(Cn > 1) stop("Coverage level must be between (0, 1)")
   
@@ -39,6 +49,16 @@ covstop <- function(comm, type = "samples", Cn = 0.90, knots = 40) {
       m <- c(1, m[-1])
       
       C <- Chat.Ind(Spec, m)
+      
+      # if(se == TRUE) {
+      #   
+      #   Prob.hat <- EstiBootComm.Ind(Spec)
+      #   
+      #   Abun.Mat <- rmultinom(nboot, n, Prob.hat)
+      #   
+      #   error.C <- qnorm(1 - (1 - 0.95) / 2) * apply(apply(Abun.Mat, 2, function(x) Chat.Ind(x, m)), 1, sd, na.rm = TRUE)
+      #   
+      # }
         
     } else if(type == "samples") {
       
@@ -65,8 +85,11 @@ covstop <- function(comm, type = "samples", Cn = 0.90, knots = 40) {
     
   } )
   
-  paste0("Number of ", type, " needed to reach ", Cn*100, "% coverage: ", as.numeric(sc[which.max(sc)]))
+  # if(se == TRUE) 
+  #   
+  #   paste0("Number of ", type, " needed to reach ", Cn*100, "% coverage: ", as.numeric(sc[which.max(sc)]), "+/-", as.numeric(error.C))
   
+  paste0("Number of ", type, " needed to reach ", Cn*100, "% coverage: ", as.numeric(sc[which.max(sc)]))
   
 }
 
@@ -117,3 +140,51 @@ Chat.Sam <- function(x, t){
   sapply(t, Sub)		
 }
 
+EstiBootComm.Ind <- function(Spec)
+{
+  Sobs <- sum(Spec > 0)   #observed species
+  n <- sum(Spec)        #sample size
+  f1 <- sum(Spec == 1)   #singleton 
+  f2 <- sum(Spec == 2)   #doubleton
+  f0.hat <- ifelse(f2 == 0, (n - 1) / n * f1 * (f1 - 1) / 2, (n - 1) / n * f1 ^ 2/ 2 / f2)  #estimation of unseen species via Chao1
+  A <- ifelse(f1>0, n*f0.hat/(n*f0.hat+f1), 1)
+  a <- f1/n*A
+  b <- sum(Spec / n * (1 - Spec / n) ^ n)
+  if(f0.hat==0){
+    w <- 0
+    if(sum(Spec>0)==1){
+      warning("This site has only one species. Estimation is not robust.")
+    }
+  }else{
+    w <- a / b      	#adjusted factor for rare species in the sample
+  }
+  Prob.hat <- Spec / n * (1 - w * (1 - Spec / n) ^ n)					#estimation of relative abundance of observed species in the sample
+  Prob.hat.Unse <- rep(a/ceiling(f0.hat), ceiling(f0.hat))  	#estimation of relative abundance of unseen species in the sample
+  return(sort(c(Prob.hat, Prob.hat.Unse), decreasing=TRUE))		  							#Output: a vector of estimated relative abundance
+}
+
+EstiBootComm.Sam <- function(Spec)
+{
+  nT <- Spec[1]
+  Spec <- Spec[-1]
+  Sobs <- sum(Spec > 0)   #observed species
+  Q1 <- sum(Spec == 1) 	#singleton 
+  Q2 <- sum(Spec == 2) 	#doubleton
+  Q0.hat <- ifelse(Q2 == 0, (nT - 1) / nT * Q1 * (Q1 - 1) / 2, (nT - 1) / nT * Q1 ^ 2/ 2 / Q2)	#estimation of unseen species via Chao2
+  A <- ifelse(Q1>0, nT*Q0.hat/(nT*Q0.hat+Q1), 1)
+  a <- Q1/nT*A
+  b <- sum(Spec / nT * (1 - Spec / nT) ^ nT)
+  
+  if(Q0.hat==0){
+    w <- 0
+    if(sum(Spec>0)==1){
+      warning("This site has only one species. Estimation is not robust.")
+    }
+  }else{
+    w <- a / b      	#adjusted factor for rare species in the sample
+  }
+  
+  Prob.hat <- Spec / nT * (1 - w * (1 - Spec / nT) ^ nT)					#estimation of detection probability of observed species in the sample
+  Prob.hat.Unse <- rep(a/ceiling(Q0.hat), ceiling(Q0.hat))  	#estimation of detection probability of unseen species in the sample
+  return(sort(c(Prob.hat, Prob.hat.Unse), decreasing=TRUE))									#Output: a vector of estimated detection probability
+}
